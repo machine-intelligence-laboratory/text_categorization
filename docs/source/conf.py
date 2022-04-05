72fe7df249e361eb99a5f4b3740784dfd37a85b5
Guince <potapov.polina@gmail.com>
2022-04-05 22:48:05 +0300

    little fix documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2022, MIL Team'
 author = 'MIL Team'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.0'
+release = '0.2.0'
 
 
 # -- General configuration ---------------------------------------------------
