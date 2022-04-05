6019bb77598898bf51e69f41457a70eb30469c0b
Guince <potapov.polina@gmail.com>
2022-04-05 19:35:49 +0300

    little fix documentation

--- ap/utils/bpe.py
+++ ap/utils/bpe.py
@@ -4,6 +4,12 @@ import youtokentome as yttm
 
 
 def load_bpe_models(bpe_path):
+    """
+    Загружает обученные BPE модели.
+
+    Args:
+        bpe_path: путь до папки с обученными BPE моделями.
+    """
     res = {}
     for model in os.listdir(bpe_path):
         res[model.split("_")[2]] = yttm.BPE(
