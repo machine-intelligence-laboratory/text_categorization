bde7843c6afb2988a67bfbb7277164e1fb8d860b
Polina Potapova <35920684+Guince@users.noreply.github.com>
2022-04-13 23:34:48 +0300

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,9 +30,13 @@ release = '0.1.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'rtds_action']
 
-# rtds_action_github_repo = "machine-intelligence-laboratory/text_categorization"
+rtds_action_github_repo = "machine-intelligence-laboratory/text_categorization"
+rtds_action_path = '../../../ap/topic_model'
+rtds_action_artifact_prefix = "my-artifact-"
+rtds_action_github_token = os.environ["GITHUB_TOKEN"]
+rtds_action_error_if_missing = False
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
