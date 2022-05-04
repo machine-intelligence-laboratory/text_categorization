d0c34610d66e68b4a575f13f226656e6f74601e7
Polina <potapova.ps@phystech.edu>
2022-05-04 15:54:46 +0300

    style fix

--- ap/train/trainer.py
+++ ap/train/trainer.py
@@ -86,7 +86,7 @@ class ModelTrainer:
         model = artm.ARTM(num_topics=artm_model_params["NUM_TOPICS"],
                           theta_columns_naming='title',
                           class_ids=modalities_with_weight,
-                          show_progress_bars=False,
+                          show_progress_bars=True,
                           dictionary=dictionary)
 
         model.scores.add(artm.SparsityThetaScore(name='SparsityThetaScore',
