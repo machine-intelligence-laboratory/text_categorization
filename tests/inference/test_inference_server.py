f2bdfb72eebae11cb11c76c7b362612ff40b841a
nikolai <aby2sz@gmail.com>
2022-06-02 13:11:26 +0300

    fix interface

--- tests/inference/test_inference_server.py
+++ tests/inference/test_inference_server.py
@@ -34,13 +34,76 @@ def artm_model():
             "три",
             "выходи"]
     doc = np.linalg.norm(np.sum([tokens[x] for x in doc]))
+    thetas = [[5.72787840e-02, 5.13726954e-02, 8.18006932e-04, 8.90530514e-01],
+                [4.87438921e-04, 6.83337098e-01, 2.91425844e-01, 2.47496193e-02]]
+    print(thetas)
     mocked_model = MagicMock()
     mocked_model.transform = Mock(
-        return_value=pd.DataFrame(index=[f'topic_{i}' for i in range(num_topic)],
+        side_effect=[pd.DataFrame(index=[f'topic_{i}' for i in range(num_topic)],
                                   columns=[
                                       "0_0",
                                   ],
-                                  data=np.random.dirichlet([0.2, 0.2, 0.2, 0.2], 4)[2])
+                                  data=thetas[0]),
+                      pd.DataFrame(index=[f'topic_{i}' for i in range(num_topic)],
+                                   columns=[
+                                       "0_0",
+                                   ],
+                                   data=thetas[1]),
+                      pd.DataFrame(index=[f'topic_{i}' for i in range(num_topic)],
+                                   columns=[
+                                       "0_0",
+                                   ],
+                                   data=thetas[1]),
+                      pd.DataFrame(index=[f'topic_{i}' for i in range(num_topic)],
+                                   columns=[
+                                       "0_0",
+                                   ],
+                                   data=thetas[1]),
+                      pd.DataFrame(index=[f'topic_{i}' for i in range(num_topic)],
+                                   columns=[
+                                       "0_0",
+                                   ],
+                                   data=thetas[1]),
+                      pd.DataFrame(index=[f'topic_{i}' for i in range(num_topic)],
+                                   columns=[
+                                       "0_0",
+                                   ],
+                                   data=thetas[1]),
+                      pd.DataFrame(index=[f'topic_{i}' for i in range(num_topic)],
+                                   columns=[
+                                       "0_0",
+                                   ],
+                                   data=thetas[1]),
+                      pd.DataFrame(index=[f'topic_{i}' for i in range(num_topic)],
+                                   columns=[
+                                       "0_0",
+                                   ],
+                                   data=thetas[1]),
+                      pd.DataFrame(index=[f'topic_{i}' for i in range(num_topic)],
+                                   columns=[
+                                       "0_0",
+                                   ],
+                                   data=thetas[1]),
+                      pd.DataFrame(index=[f'topic_{i}' for i in range(num_topic)],
+                                   columns=[
+                                       "0_0",
+                                   ],
+                                   data=thetas[1]),
+                      pd.DataFrame(index=[f'topic_{i}' for i in range(num_topic)],
+                                   columns=[
+                                       "0_0",
+                                   ],
+                                   data=thetas[1]),
+                      pd.DataFrame(index=[f'topic_{i}' for i in range(num_topic)],
+                                   columns=[
+                                       "0_0",
+                                   ],
+                                   data=thetas[1]),
+                      pd.DataFrame(index=[f'topic_{i}' for i in range(num_topic)],
+                                   columns=[
+                                       "0_0",
+                                   ],
+                                   data=thetas[1])]
     )
     mocked_model.get_phi = Mock(
         return_value=pd.DataFrame(index=list(tokens.keys()),
