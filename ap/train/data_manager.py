c6659fb85bce6c2e38ef6974bc129eb8967f9726
nikolai <aby2sz@gmail.com>
2022-06-03 17:09:51 +0300

    fix interface

--- ap/train/data_manager.py
+++ ap/train/data_manager.py
@@ -97,7 +97,7 @@ class ModelDataManager:
 
     def generate_background_batches(self):
         import artm
-        if os.path.exists(self.new_background_path):
+        if not os.path.exists(self.new_background_path):
             return
         with tempfile.TemporaryDirectory(dir=self._data_dir) as temp_dir:
             batch_vectorizer = artm.BatchVectorizer(data_path=self.new_background_path, data_format='vowpal_wabbit',
