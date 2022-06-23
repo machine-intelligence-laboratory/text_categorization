e23782484ade4abdf0de43191c48515f41778e92
Polina <potapova.ps@phystech.edu>
2022-06-23 16:12:01 +0300

    little fix

--- inference.sh
+++ inference.sh
@@ -1,5 +1,3 @@
-export MODEL_PATH=/home/antiplagiat/models/PLSA_L14_V20000_TOP100_E1_G0_T10
-export BPE_PATH=/home/antiplagiat/bpe_models
-export RUBRIC_PATH=/home/antiplagiat/rubrics
+export MODEL_PATH=./best_model_avr_rubric_size_25/20220526_211543
 
 docker-compose -f docker-compose.yml build && docker-compose -f docker-compose.yml up
--- train.sh
+++ train.sh
@@ -1,4 +1,4 @@
-export CONFIG=/home/antiplagiat/config
-export DATA_PATH=/home/antiplagiat/data
+export CONFIG=./ap/utils/experiment_config.yml
+export DATA_PATH=/data/datasets/Antiplagiat/
 
 docker-compose -f docker-compose-train.yml build && docker-compose -f docker-compose-train.yml up
